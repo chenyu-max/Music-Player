f5e931774c3b1709b0c0693ec00c6d4cfb40df71
chenyu-max <56399206+chenyu-max@users.noreply.github.com>
2021-05-23 14:03:26 +0800

    commit2

--- gulpfile.js
+++ gulpfile.js
@@ -13,59 +13,59 @@ const imgMin = require('gulp-imagemin');
 const connect = require('gulp-connect');
 
 const folder = {
-	src: 'src/',
-	dist: 'dist/'
+    src: 'src/',
+    dist: 'docs/'
 }
 
 function html() {
-	return src(folder.src + 'html/*')
-		.pipe(htmlClean())
-		.pipe(dest(folder.dist + 'html/'))
-		.pipe(connect.reload());
+    return src(folder.src + 'html/*')
+        .pipe(htmlClean())
+        .pipe(dest(folder.dist))
+        .pipe(connect.reload());
 }
 
 function css() {
-	return src(folder.src + 'css/*')
-		.pipe(less())
-		.pipe(cleanCss())
-		.pipe(dest(folder.dist + 'css/'))
-		.pipe(connect.reload());
+    return src(folder.src + 'css/*')
+        .pipe(less())
+        .pipe(cleanCss())
+        .pipe(dest(folder.dist + 'css/'))
+        .pipe(connect.reload());
 }
 
 function js() {
-	return src(folder.src + 'js/*')
-		/* .pipe(stripDebug())
-		.pipe(uglify()) */
-		.pipe(dest(folder.dist + 'js/'))
-		.pipe(connect.reload());
+    return src(folder.src + 'js/*')
+        /* .pipe(stripDebug())
+        .pipe(uglify()) */
+        .pipe(dest(folder.dist + 'js/'))
+        .pipe(connect.reload());
 }
 
 function image() {
-	return src(folder.src + 'images/*')
-		.pipe(imgMin())
-		.pipe(dest(folder.dist + 'images/'))
+    return src(folder.src + 'images/*')
+        .pipe(imgMin())
+        .pipe(dest(folder.dist + 'images/'))
 }
 
 function server(cb) {
-	connect.server({
-		port: '1573',
-		livereload: true,	//自动刷新
-	});
+    connect.server({
+        port: '1573',
+        livereload: true, //自动刷新
+    });
 
-	cb();
+    cb();
 }
 
-watch(folder.src+'html/*',function(cb){
-	html();
-	cb();
+watch(folder.src + 'html/*', function(cb) {
+    html();
+    cb();
 });
-watch(folder.src+'css/*',function(cb){
-	css();
-	cb();
+watch(folder.src + 'css/*', function(cb) {
+    css();
+    cb();
 });
-watch(folder.src+'js/*',function(cb){
-	js();
-	cb();
+watch(folder.src + 'js/*', function(cb) {
+    js();
+    cb();
 });
 
 exports.default = series(html, css, js, image, server)
